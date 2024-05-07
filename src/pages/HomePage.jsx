3f8bd8b360190c27f74aaf5534f0e75b787193da
swifty-sketch <marsel.davletov@chasacademy.se>
2024-05-07 17:29:02 +0200

    1

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,19 @@
+import Hero from "../comps/Hero"
+import HomeCards from "../comps/HomeCards"
+import JobListings from "../comps/JobListings"
+import ViewAllJobs from "../comps/ViewAllJobs"
+
+
+const HomePage = () => {
+  return (
+    <>
+    <Hero />
+    <HomeCards/>
+    <JobListings/>
+    <ViewAllJobs/>
+    
+    </>
+  )
+}
+
+export default HomePage
